8bf83056b9933280c30ec2984085ce4c37d7e439
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-01-06 13:54:40 +0100

    [ travis ] setting up the build

---
+++ travis/install_agda.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.1" ]; then
+  cabal update
+  cabal install alex happy cpphs
+  git clone git@github.com:agda/agda --depth=1
+  cd agda
+  cabal install
+  cd ..
+  mkdir -p $HOME/.agda
+  cp libraries-2.6.0 $HOME/.agda/
+  cd $HOME/.agda/
+  git clone git@github.com:agda/agda-stdlib experimental --depth=1
+fi
