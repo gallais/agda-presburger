eec59a855aae7a36057915b77bc6e901c5d83bdc
G. Allais <guillaume.allais@ens-lyon.org>
2019-01-07 21:00:20 +0100

    [ travis ] Typechecking the whole project (#2)

---
+++ travis/install_agda.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.6.0" ]; then
+  cabal update
+  cabal install alex happy cpphs
+  git clone https://github.com/agda/agda --depth=1
+  cd agda
+  cabal install
+  cd ..
+  mkdir -p $HOME/.agda
+  cp libraries-2.6.0 $HOME/.agda/
+  cd $HOME/.agda/
+  git clone -b experimental https://github.com/agda/agda-stdlib --depth=1
+fi
